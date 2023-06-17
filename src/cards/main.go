92065955ad6448684a9eaedf6cf2b43cc5a4b9a4
Steven Yan <yunsheng.yan@walmart.com>
2023-06-16 20:44:44 -0700

    array and slice

--- src/cards/main.go
+++ src/cards/main.go
@@ -3,10 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
-	fmt.Println(card)
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+	// fmt.Println(cards)
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
 	return "Five of Diamonds"
 }
+
+// ❯ ./main
+// 0 Ace of Diamonds
+// 1 Five of Diamonds
+// 2 Six of Spades
