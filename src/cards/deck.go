77c88dd09db450c620983025ba1b010d387da43b
Steven Yan <yunsheng.yan@walmart.com>
2023-06-17 21:10:27 -0700

    deck shuffle function and randomization

--- src/cards/deck.go
+++ src/cards/deck.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -54,4 +56,15 @@ func newDeckFromFile(filename string) deck {
 
 	s := strings.Split(string(bs), ",")
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source) // https://pkg.go.dev/math/rand@go1.20.5#New
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
