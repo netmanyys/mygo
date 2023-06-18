d283d67264f964584f27926ddd2eaea4e8a6a22d
Steven Yan <yunsheng.yan@walmart.com>
2023-06-17 17:48:35 -0700

    newDeck function with deck type

--- src/cards/deck.go
+++ src/cards/deck.go
@@ -6,6 +6,20 @@ import "fmt"
 // Which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
 // this is the receiver function of type deck
 func (d deck) print() {
 	for i, card := range d {
