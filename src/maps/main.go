0874bdb885184c229f898f9d3a94f784ef26b0e2
Steven Yan <yunsheng.yan@walmart.com>
2023-06-21 18:01:49 -0700

    maps again and []

--- src/maps/main.go
+++ src/maps/main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var colors map[string]string
+	colors := make(map[int]string)
 
+	colors[10] = "#ffffff"
+
+	delete(colors, 10)
 	fmt.Println(colors)
 }
 
